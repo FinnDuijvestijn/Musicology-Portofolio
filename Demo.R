88662caf5addedb398b0ec17c221b7f5aa9af749
FinnDuijvestijn <finnduijv@gmail.com>
2023-02-27 09:54:16 +0100

    html

--- Demo.R
+++ Demo.R
@@ -1,19 +1,26 @@
 library(tidyverse)
 library(spotifyr)
 
-grammy <- get_playlist_audio_features("", "4kQovkgBZTd8h2HCM3fF31")
-edison <- get_playlist_audio_features("", "37i9dQZF1DX8mnKbIkppDf")
-disney <- get_playlist_audio_features("", "37i9dQZF1DX8C9xQcOrE6T")
+Sys.setenv(SPOTIFY_CLIENT_ID="34c9d0d698064bf886a78b343db5445b")
+Sys.setenv(SPOTIFY_CLIENT_SECRET="d0e93595f1ee411bb21938c5cb50d247")
+
+access_token <- get_spotify_access_token()
+
+pop <- get_playlist_audio_features("", "2AipD5zTjWWM13wk2hZmej")
+phonk <- get_playlist_audio_features("", "37i9dQZF1DWWY64wDtewQt")
+rap <- get_playlist_audio_features("", "37i9dQZF1DX76t638V6CA8")
 
 awards <-
   bind_rows(
-    grammy |> mutate(category = "Grammys"),
-    edison |> mutate(category = "Edisons"),
-    disney |> mutate(category = "Disney")
+    pop |> mutate(category = "Pop"),
+    phonk |> mutate(category = "Phonk"),
+    rap |> mutate(category = "Rap")
     
   )
 
-grammy |> ggplot(aes(x = energy)) + geom_histogram(binwidth = 0.1)
+pop |> ggplot(aes(x = energy)) + geom_histogram(binwidth = 0.1)
+
+rap |> ggplot(aes(x = valence)) + geom_density()
 
 awards |>
   ggplot(aes(x = energy)) +
@@ -28,9 +35,9 @@ awards |>
   ggplot(aes(x = category, y = energy)) +
   geom_violin()
 
-grammy |> ggplot(aes(x = valence, y = energy)) + geom_point() + geom_smooth()
+rap |> ggplot(aes(x = valence, y = energy)) + geom_point() + geom_smooth()
 
-grammy |> ggplot(aes(x = valence, y = energy)) + geom_jitter() + geom_smooth()
+rap |> ggplot(aes(x = valence, y = energy)) + geom_jitter() + geom_smooth()
 
 awards |>                    # Start with awards.
   mutate(
@@ -46,26 +53,26 @@ awards |>                    # Start with awards.
   ) +
   geom_point() +              # Scatter plot.
   geom_rug(linewidth = 0.1) + # Add 'fringes' to show data distribution.
-  geom_text(                  # Add text labels from above.
-    aes(
-      x = valence,
-      y = energy,
-      label = label
-    ),
-    data = 
-      tibble(
-        label = c("Altijd wel iemand", "ENERGY"),
-        category = c("Edisons", "Grammys"),
-        valence = c(0.090, 0.123),
-        energy = c(0.101, 0.967)
-      ),
-    colour = "black",         # Override colour (not mode here).
-    size = 3,                 # Override size (not loudness here).
-    hjust = "left",           # Align left side of label with the point.
-    vjust = "bottom",         # Align bottom of label with the point.
-    nudge_x = -0.05,          # Nudge the label slightly left.
-    nudge_y = 0.02            # Nudge the label slightly up.
-  ) +
+  # geom_text(                  # Add text labels from above.
+  #   aes(
+  #     x = valence,
+  #     y = energy,
+  #     label = label
+  #   ),
+  #   data =
+  #     tibble(
+  #       label = c("Altijd wel iemand", "ENERGY"),
+  #       category = c("Edisons", "Grammys"),
+  #       valence = c(0.090, 0.123),
+  #       energy = c(0.101, 0.967)
+  #     ),
+  #   colour = "black",         # Override colour (not mode here).
+  #   size = 3,                 # Override size (not loudness here).
+  #   hjust = "left",           # Align left side of label with the point.
+  #   vjust = "bottom",         # Align bottom of label with the point.
+  #   nudge_x = -0.05,          # Nudge the label slightly left.
+  #   nudge_y = 0.02            # Nudge the label slightly up.
+  # ) +
   facet_wrap(~ category) +    # Separate charts per playlist.
   scale_x_continuous(         # Fine-tune the x axis.
     limits = c(0, 1),
@@ -92,3 +99,47 @@ awards |>                    # Start with awards.
     colour = "Mode"
   )
 
+
+
+
+
+
+
+
+
+awards |>                    # Start with awards.
+  ggplot(                     # Set up the plot.
+    aes(
+      x = valence,
+      y = energy,
+      size = loudness,
+      colour = energy
+    )
+  ) +
+  geom_point() +              # Scatter plot.
+  geom_rug(linewidth = 0.2) + # Add 'fringes' to show data distribution.
+
+facet_wrap(~ category) +    # Separate charts per playlist.
+  scale_x_continuous(         # Fine-tune the x axis.
+    limits = c(0, 1),
+    breaks = c(0, 0.50, 1),   # Use grid-lines for quadrants only.
+    minor_breaks = NULL       # Remove 'minor' grid-lines.
+  ) +
+  scale_y_continuous(         # Fine-tune the y axis in the same way.
+    limits = c(0, 1),
+    breaks = c(0, 0.50, 1),
+    minor_breaks = NULL
+  ) +
+  # scale_colour_brewer(        # Use the Color Brewer to choose a palette.
+  #   type = "qual",            # Qualitative set.
+  #   palette = "Paired"        # Name of the palette is 'Paired'.
+  # ) +
+  scale_size_continuous(      # Fine-tune the sizes of each point.
+    trans = "exp",            # Use an exp transformation to emphasise loud.
+    guide = "none"            # Remove the legend for size.
+  ) +
+  theme_light() +             # Use a simpler theme.
+  labs(                       # Make the titles nice.
+    x = "Valence",
+    y = "Energy",
+  )
